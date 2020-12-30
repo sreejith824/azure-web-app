fab417c0ab05b4d988b6e8ded1c6431cd07295ef
sreejith824 <sreejith824@gmail.com>
2020-12-30 12:47:33 +0530

    Key vault implementation

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
+ 
+const credential = new DefaultAzureCredential();
+ 
+const url = "https://az303helloworldkayvault.vault.azure.net/";
+ 
+const client = new SecretClient(url, credential);
+ 
+const secretName = "secretuser";
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  getSecretValue().then(response => {
+    res.send('respond with a resource : ' + response );
+  });
 });
+
+async function getSecretValue() {
+  const latestSecret = await client.getSecret(secretName);
+  console.log(`Latest version of the secret ${secretName}: `, latestSecret);
+  const specificSecret = await client.getSecret(secretName, { version: latestSecret.properties.version});
+  console.log(`The secret ${secretName} at the version ${latestSecret.properties.version}: `, specificSecret);
+  return "The secret "+ specificSecret.value;
+}
 
 module.exports = router;
